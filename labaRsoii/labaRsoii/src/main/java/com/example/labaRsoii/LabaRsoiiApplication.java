3f870d46ee65880df42296dfbc09d13409593067
Kryukov.SA <sergey.krykov.2806@gmail.com>
2022-10-02 18:04:39 +0300

    create_spring_app

---
+++ labaRsoii/labaRsoii/src/main/java/com/example/labaRsoii/LabaRsoiiApplication.java
@@ -0,0 +1,13 @@
+package com.example.labaRsoii;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabaRsoiiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabaRsoiiApplication.class, args);
+	}
+
+}
