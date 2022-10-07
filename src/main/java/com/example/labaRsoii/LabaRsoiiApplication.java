6031a1a3c92ad1037b9b1798bc6758bd67ec6b0e
Kryukov.SA <sergey.krykov.2806@gmail.com>
2022-10-07 22:51:32 +0300

    add dockerfile

--- src/main/java/com/example/labaRsoii/LabaRsoiiApplication.java
+++ src/main/java/com/example/labaRsoii/LabaRsoiiApplication.java
@@ -2,10 +2,13 @@ package com.example.labaRsoii;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 @SpringBootApplication
-//@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@PropertySources({
+		@PropertySource("classpath:application.properties"),
+		@PropertySource("classpath:application-local.properties")})
 public class LabaRsoiiApplication {
 
 	public static void main(String[] args) {
